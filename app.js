6b69fe4c328b28e4365d7c8179f8a3a5a3835434
Kat <63946129+khhuang7@users.noreply.github.com>
2024-02-23 23:46:29 -0500

    Show library display and add display function

--- app.js
+++ app.js
@@ -11,3 +11,27 @@ function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(new Book(title, author, pages, read));
 }
 
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, true);
+addBookToLibrary("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", 216, false);
+
+function displayBooks() {
+  const library = document.getElementById("library");
+  for (let i = 0; i < myLibrary.length; i++) {
+    let book = myLibrary[i];
+    let bookStatus = 
+
+    card = document.createElement("div");
+    card.setAttribute("class", "book-card");
+    card.innerHTML = `<div class="book-cover-details">
+        <div class="book-title">${book.title}</div>
+        by
+        <div class="book-author">${book.author}</div>
+      </div>
+      <div class="book-pages">${book.pages} pages</div>
+      <div class="book-status ${(book.read === true) ? "read" : "not-read"}">
+        ${(book.read === true) ? "Not read yet" : "Completed"}</div>`;
+    library.appendChild(card);
+  }
+}
+
+displayBooks();
